626ec8106bee95dafccf59d381cdbd45d2d768aa
IPampurin <I.Pampurin@yandex.ru>
2025-07-28 20:57:18 +0500

    repeat

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,16 +3,15 @@ package server
 import (
 	"fmt"
 	"net/http"
-	"os"
 )
 
 func Run() error {
 
 	// назначим порт (80 - чтобы доступ был на localhost)
-	port, ok := os.LookupEnv("SQNStest_PORT")
-	if !ok {
-		port = "80"
-	}
+	//	port, ok := os.LookupEnv("SQNStest_PORT")
+	//	if !ok {
+	port := "80"
+	//	}
 
 	http.Handle("/", http.FileServer(http.Dir("web")))
 
