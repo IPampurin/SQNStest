7e20ed2f2c520b08b87ee0829315450005ac2083
IPampurin <I.Pampurin@yandex.ru>
2025-07-28 23:06:59 +0500

    workflow with TestServer

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -11,7 +11,7 @@ func Run() error {
 	// назначим порт (80 - чтобы доступ был на localhost)
 	port, ok := os.LookupEnv("SQNStest_PORT")
 	if !ok {
-		port = "80"
+		port = "8080"
 	}
 
 	http.Handle("/", http.FileServer(http.Dir("web")))
